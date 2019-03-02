87e09128d9cf041f6601505b3d972e47ccf8cc69
donis <danis.danilyuk@gmail.com>
2019-03-02 15:07:57 +0200

    v_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
+        /*
+             8208
+            /2 = 0
+            /3 = 0
+            /5 = 3
+            /7 = 4
+            /11 = 2
+            /17 = 14
+         */
 
+        String sentences = "Hello my dad. I wanna be successful! And it is very hard.";
+        System.out.println("Text: \n" + sentences);
+
+
+        // Spliting
+        StringBuilder sentence = new StringBuilder(sentences);
+        String delimeter = "\\s";
+        String[] arr_words = sentence.toString().split(delimeter);
+
+
+        // Show result
+        System.out.println("Edited text: ");
+        for (String word : arr_words) {
+
+            char[] arr_chars = word.toCharArray();
+            for (int k = 0; k < arr_chars.length; k++) {
+                char temp = arr_chars[0];
+                if (k == 0) {
+                    System.out.print(arr_chars[k]);
+                }
+                if (temp != arr_chars[k]) {
+                    System.out.print(arr_chars[k]);
+                }
+            }
+
+            System.out.print(" ");
+        }
     }
 }
