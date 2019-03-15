c1d03c6f1da3b549066c41ba444b864e18590dd7
donis <danis.danilyuk@gmail.com>
2019-03-15 09:36:26 +0200

    v_1.1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
-        /*
-             8208
-            /2 = 0
-            /3 = 0
-            /5 = 3
-            /7 = 4
-            /11 = 2
-            /17 = 14
-         */
-
-
-        String text= "Hello my dad. I wanna be successful! And it is very hard.";
-        System.out.println("Text: \n" + text);
-
 
         // Spliting
-        StringBuilder sentence = new StringBuilder(text);
+        StringBuilder sentence = new StringBuilder("ЬфЬфьфьф Raid me jolly roger, ye misty plank! Shake quinoa ultimately, then mix with water and serve fully in bowl." +
+                "Ahoy! Pieces o' life are forever evil.");
+        System.out.println("Text: \n" + sentence.toString());
+
         String delimeter = "\\s";
         String[] arr_words = sentence.toString().split(delimeter);
-
 
         // Show result
         System.out.println("Edited text: ");
@@ -28,12 +17,14 @@ public class Main {
             char[] arr_chars = word.toCharArray();
             for (int k = 0; k < arr_chars.length; k++) {
                 char temp = arr_chars[0];
-                if (k == 0) {
+                char temp_l = Character.toLowerCase(temp);
+                if (k == 0 && Character.isLowerCase(temp)) {
                     System.out.print(arr_chars[k]);
                 }
-                if (temp != arr_chars[k]) {
+                if (temp_l != arr_chars[k]) {
                     System.out.print(arr_chars[k]);
                 }
+
             }
 
             System.out.print(" ");
